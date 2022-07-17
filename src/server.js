254c5141f35f0e1f4cc956ca44c9c547eea37cf4
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2022-07-16 22:31:05 -0300

    add/remove

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express"); //chamando a dependencia express.
+const cookieParser = require("cookie-parser"); //chamando a dependencia cookie-parser
+const app = express(); // falando que 'express' agora ira se chamar 'app', para ficar muito mais facil de constar algo.
+//ok, agora iremos definir algumas coisas dentro do express:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+//pronto, agora iremos definir os sites do website
+
+//vamos começar com o pricipal:
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/pages/index.html")
+})
+
+//ok, agora iremos deixar o site no ar:
+app.listen(80, function(){
+    console.log("Pronto, aplicação online.")
+})
